96e14ef5ea050fe63cdbfdc3bb4265a17d228074
MUNEER461 <60612345+MUNEER461@users.noreply.github.com>
2020-09-09 14:58:20 +0530

    Add files via upload

---
+++ allpkg.sh
@@ -0,0 +1,37 @@
+#!bin/bash
+#let's start
+apt update
+apt upgrade
+pkg update
+pkg upgrade
+pkg install git
+apt install proot
+pkg install nano
+apt install apache2
+pkg install bash
+pkg install python
+pkg install python2
+pkg install openssh
+pkg install php
+pkg install ruby
+pkg install wget
+pkg install nmap
+pkg install sql
+pkg install nginx
+apt install zip
+apt install unzip
+pkg install hydra
+pip install wafw00f
+pkg install curl
+pkg install ncurses-utils
+pip install -r requirements.txt
+pip2 install -r requirements.txt
+pip2 install requests
+pip install requests
+pip install netaddr
+pip install argparse
+pkg install root-repo
+pkg install unstable-repo
+pkg install x11-repo
+pkg install ruby clang
+pip install mechanize requests bs4
