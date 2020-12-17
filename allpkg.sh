8ff7c56f2b0f7cf7771016b1f384f52145a9edb0
MUNEER461 <60612345+MUNEER461@users.noreply.github.com>
2020-12-17 14:57:06 +0530

    allpkg.sh

--- allpkg.sh
+++ allpkg.sh
@@ -1,5 +1,71 @@
-#!bin/bash
-#let's start
+#!/bin/bash
+clear
+
+
+echo -e "
+       ______   __       __  ________
+      /      \ /  \     /  |/        |
+      /€€€€€€  |€€  \   /€€ |€€€€€€€€/ 
+      €€ |  €€/ €€€  \ /€€€ |€€ |__    
+      €€ |      €€€€  /€€€€ |€€    |   
+      €€ |   __ €€ €€ €€/€€ |€€€€€/    
+      €€ \__/  |€€ |€€€/ €€ |€€ |_____ 
+      €€    €€/ €€ | €/  €€ |€€       |
+      €€€€€€€/   €€/     €€/ €€€€€€€€/ 
+                                 
+                                 "
+
+printf "  ⚠️ warning ⚠️  "
+echo -e
+
+
+echo -e
+
+
+echo -e "🙏 Dont try to edit my script if you edit means you cannot become programer Thankyou 🙏"
+
+echo -e
+
+echo -e
+
+echo -e "==========={👇 ♕ If you want follow means click Link bleow ♕👇}==========="
+
+echo -e 
+
+
+echo -e " ➜  facebookcebook https://www.facebook.com/muneer.uddin.104"
+
+
+echo -e " ➜  Instagram : https://www.instagram.com/muneer_lovely/"
+
+
+echo -e  " ➜  outube : https://www.youtube.com/channel/UC_CkXiuB3XA-az09Mmi8Wsg"
+
+
+echo -e   " ➜  Github : https://github.com/MUNEER461/"
+echo -e
+
+echo -e " =================={🙏Thank you for using our script 🙏}==================="
+echo -e
+
+echo " ➜ press 1 : To install small  packages ❨ 0.9 GB Storage  ❩"
+echo
+echo " ➜ press 2 : To install  metasploit package ❨ 1.5GB Storage  ❩ "
+echo 
+echo " ➜ press 3 : To exit  "
+echo
+read ch
+if [ $ch -eq 1 ];then
+
+clear
+
+echo "➣It will take some Time "
+echo
+echo "➣Please Dont do anything"
+echo
+echo "➣lets start             "
+echo
+
 apt update
 apt upgrade
 pkg update
@@ -7,7 +73,7 @@ pkg upgrade
 pkg install git
 apt install proot
 pkg install nano
-apt install apache2
+apt install apache2@
 pkg install bash
 pkg install python
 pkg install python2
@@ -21,6 +87,8 @@ pkg install nginx
 apt install zip
 apt install unzip
 pkg install hydra
+pkg install nmap
+pkg install python3
 pip install wafw00f
 pkg install curl
 pkg install ncurses-utils
@@ -33,5 +101,32 @@ pip install argparse
 pkg install root-repo
 pkg install unstable-repo
 pkg install x11-repo
+pkg install aircrack-ng
+pkg install while
 pkg install ruby clang
 pip install mechanize requests bs4
+exit 
+read ch
+elif [ $ch -eq 2 ];then
+echo ➣ use speed internet connection other wise  it will Download very slow 
+echo -e
+
+echo -e
+
+echo ➣ This installation process  It will up 1 Hour depend on your internet connection speed
+echo -e
+
+echo -e
+
+
+pkg install metasploit
+exit
+clear
+
+read ch 
+elif [ $ch -eq 3 ];then
+clear
+exit
+clear
+fi
+done
